b2a1e508a9e84b2c3278bed99db70699a6b9948d
Sanka <vaeceby2@qq.com>
2022-11-04 10:10:36 +0800

    update | new palyground views

--- components/playground/index.js
+++ components/playground/index.js
@@ -1,4 +1,5 @@
 // components/playground/index.js
+const app = getApp()
 Component({
   /**
    * 组件的属性列表
@@ -11,35 +12,97 @@ Component({
    * 组件的初始数据
    */
   data: {
-    isActive:false,
-    user:null
+    isActive: false,
+    user: null,
+    isFocus: false,
+    postList: [],
+    pContent: '',
+    pTitle: '测试标题'
+
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    clickBall(){
+    clickBall() {
       this.setData({
-        isActive:!this.data.isActive
+        isActive: !this.data.isActive
       })
     },
-    onClose(){
+    onClose() {
       wx.showLoading({
         title: '加载中...',
       })
-      setTimeout(()=>{
-        wx.hideLoading({
-          success: (res) => {
-            wx.showToast({
-              title: '成功',
-            })
-            this.setData({
-              isActive:false
-            })
-          },
-        })
-      },1000)
+      console.log(this.data)
+      // setTimeout(()=>{
+      const _this = this
+      wx.request({
+        url: `${app.globalData.baseUrl}/playground/post`,
+        method: "POST",
+        data: {
+          uId: _this.data.user.uId,
+          pContent: _this.data.pContent,
+          pTitle: _this.data.pTitle
+        },
+        success: () => {
+          wx.hideLoading({
+            success: (res) => {
+              wx.showToast({
+                title: '成功',
+              })
+              this.setData({
+                isActive: false
+              })
+            },
+          })
+        }
+      })
+
+      // },1000)
+    },
+    setFocusName() {
+      this.setData({
+        isFocus: true
+      })
+    },
+    removeFocusName(e) {
+      console.log(e)
+      this.setData({
+        isFocus: false,
+        pContent: e.detail.value
+      })
+    },
+    pullDownRefresh() {
+      console.log('下拉刷新....')
+    },
+    loadMore() {
+      console.log('加载更多....')
+    },
+    initData() {
+      wx.showLoading({
+        title: '加载中...',
+      })
+
+      // setTimeout(()=>{
+      const _this = this
+      wx.request({
+        url: `${app.globalData.baseUrl}/playground`,
+        method: "GET",
+
+        success: (res) => {
+          console.log(res)
+          _this.setData({
+            postList: res.data.playgroundList
+          })
+          wx.hideLoading({
+            success: (res) => {
+
+
+            },
+          })
+        }
+      })
     }
   },
   lifetimes: {
@@ -49,6 +112,7 @@ Component({
       this.setData({
         user: token.userInfo
       })
+      this.initData()
     },
   },
 })
