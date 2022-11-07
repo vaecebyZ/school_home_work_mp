352bdbd48241128f666ff503ec904adc6471829e
Sanka <vaeceby2@qq.com>
2022-11-07 20:59:48 +0800

    new | playground build

--- components/playground/index.js
+++ components/playground/index.js
@@ -13,12 +13,13 @@ Component({
    */
   data: {
     isActive: false,
+    isLoading: false,
+    page: 0,
     user: null,
     isFocus: false,
     postList: [],
     pContent: '',
     pTitle: '测试标题'
-
   },
 
   /**
@@ -30,12 +31,44 @@ Component({
         isActive: !this.data.isActive
       })
     },
+    titleInput(event) {
+      this.setData({
+        pTitle: event.detail.value
+      })
+    },
+    upPost(e) {
+      wx.showLoading({
+        title: '加载中...',
+      })
+      console.log(e.currentTarget.dataset)
+      // const _this = this
+      wx.request({
+        url: `${app.globalData.baseUrl}/playground/up`,
+        method: "POST",
+        data: {
+          pId: e.currentTarget.dataset.pid,
+          ups: e.currentTarget.dataset.ups
+        },
+        success: () => {
+          wx.hideLoading({
+            success: (res) => {
+              wx.showToast({
+                title: '成功',
+              })
+              this.setData({
+                isActive: false
+              })
+              this.initData()
+            },
+          })
+        }
+      })
+    },
     onClose() {
       wx.showLoading({
         title: '加载中...',
       })
       console.log(this.data)
-      // setTimeout(()=>{
       const _this = this
       wx.request({
         url: `${app.globalData.baseUrl}/playground/post`,
@@ -54,12 +87,11 @@ Component({
               this.setData({
                 isActive: false
               })
+              this.initData()
             },
           })
         }
       })
-
-      // },1000)
     },
     setFocusName() {
       this.setData({
@@ -70,35 +102,76 @@ Component({
       console.log(e)
       this.setData({
         isFocus: false,
-        pContent: e.detail.value
       })
     },
     pullDownRefresh() {
-      console.log('下拉刷新....')
+      console.log('下拉刷新....');
+      this.setData({
+        isLoading: true,
+        page: 0
+      })
+      this.initData()
     },
     loadMore() {
+      this.setData({
+        page: this.data.page += 1
+      })
+      this.initData()
       console.log('加载更多....')
+    },
+    contentInput(event) {
+      this.setData({
+        pContent: event.detail.value
+      })
     },
     initData() {
       wx.showLoading({
         title: '加载中...',
       })
 
-      // setTimeout(()=>{
       const _this = this
       wx.request({
         url: `${app.globalData.baseUrl}/playground`,
         method: "GET",
-
+        data: {
+          page: this.data.page
+        },
         success: (res) => {
           console.log(res)
-          _this.setData({
-            postList: res.data.playgroundList
-          })
+          if (res.data.playgroundList.length == 0 && this.data.page != 0) {
+            wx.showToast({
+              title: '没有更多了！',
+              icon: 'error'
+            })
+            this.setData({
+              page: this.data.page -= 1,
+              pContent: '',
+              pTitle: ''
+            })
+          } else {
+            if (this.data.page > 0) {
+              _this.setData({
+                postList: _this.data.postList.concat(res.data.playgroundList),
+                pContent: '',
+                pTitle: ''
+              })
+            } else {
+              _this.setData({
+                postList: res.data.playgroundList,
+                pContent: '',
+                pTitle: ''
+              })
+            }
+
+          }
+
           wx.hideLoading({
             success: (res) => {
-
-
+              setTimeout(() => {
+                this.setData({
+                  isLoading: false
+                })
+              }, 1000)
             },
           })
         }
