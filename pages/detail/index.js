50440dde02ac5134913e032dea9c0a379fdbb5af
Sanka <vaeceby2@qq.com>
2022-11-10 22:43:19 +0800

    new | some page update

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -6,14 +6,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    post: null,
+    comment: '',
+    user: null,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    console.log();
+
+    // 获取用户缓存
+    const token = wx.getStorageSync('token');
+    this.setData({
+      user: token.userInfo
+    })
+    this.init()
+
+  },
+  init() {
     wx.showLoading({
       title: '加载中...',
     })
@@ -27,11 +38,66 @@ Page({
         data
       }) => {
         console.log(data);
+        this.setData({
+          post: data.playgroundDetail
+        })
         wx.hideLoading()
       }
     })
+  },
+  startTouched() {
+    // console.log('触摸hajime')
+    this.setData({
+      clicked: true
+    })
 
+    if (this.data.comment) {
+      wx.request({
+        url: `${app.globalData.baseUrl}/playground/comment`,
+        method: "POST",
+        data: {
+          comment: this.data.comment,
+          pId: this.data.post.pId,
+          uId: this.data.user.uId,
+        },
+        success: ({
+          data
+        }) => {
+          if (data.success) {
+            wx.showToast({
+              title: '评论成功了😲',
+              icon: "success"
+            })
+            this.setData({
+              comment: ''
+            })
+            this.init()
+            // this.getCommentByVideo()
+          } else {
+            wx.showToast({
+              title: '出了一点问题.',
+              icon: "error"
+            })
+          }
+        }
+      })
+    } else {
+      wx.showToast({
+        title: '请输入内容！',
+        icon: 'none'
+      })
+    }
   },
 
-
+  endTouched() {
+    // console.log('触摸owari')
+    this.setData({
+      clicked: false
+    });
+  },
+  getComment(e) {
+    this.setData({
+      comment: e.detail.value
+    })
+  },
 })
